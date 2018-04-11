79df156499cabb4d553828c821aafdaf3d25472d
Danil Beltyukov <beltyukovdanil@gmail.com>
2018-04-11 15:49:07 +0300

    fix dockerfile; fix puppeterr args; fix build

--- Dockerfile
+++ Dockerfile
@@ -1,11 +1,13 @@
-FROM keymetrics/pm2:latest-alpine
-
-RUN apk add yarn && \
-    yarn install && \
-    yarn build
+FROM keymetrics/pm2:latest-latest-stretch
 
 WORKDIR /app
 
 COPY . .
+
+RUN curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add - && \
+    echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list && \
+    apt update && apt install --no-install-recommends yarn && \
+    yarn install && \
+    yarn build
 
 CMD ["pm2-runtime","start","ecosystem.config.js"]
--- src/saveToPdf.js
+++ src/saveToPdf.js
@@ -1,10 +1,14 @@
 import puppeteer from 'puppeteer';
 
 export default async (url, path) => {
-  const browser = await puppeteer.launch();
+  const browser = await puppeteer.launch({
+    args: ['--no-sandbox', '--disable-setuid-sandbox'],
+  });
   const page = await browser.newPage();
   await page.setUserAgent('Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3325.181 Safari/537.36');
-  await page.goto(url, { waitUntil: 'networkidle2' });
+  await page.goto(url, {
+    waitUntil: 'networkidle2',
+  });
   await page.emulateMedia('screen');
   await page.pdf({
     path,
